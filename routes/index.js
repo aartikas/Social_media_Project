c81b0c49a2ec2cebefc3608c7c8dfd48e0e9e4fb
aartikas <aartika15082@iiitd.ac.in>
2023-10-01 11:07:03 +0530

    Creation of Partials and Layouts

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
+
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 console.log("Router loaded");
 
 router.get('/',homeController.home)
+
+//for any routes, access here router.use('/routerName',require(./routerFile))
+router.use('/users', require('./routes'))
 
 
 
