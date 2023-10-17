3cbe1be61052f59bbb6c1ecae36dc983922a9c09
aartikas <aartika15082@iiitd.ac.in>
2023-10-17 14:51:12 +0530

    Addition of Posts Section after authentication

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,29 @@
-module.exports.home = function(req,res){
+const Post = require('../models/posts');
+
+
+module.exports.home = async function (req,res){
    // return res.end('<h1> Express is up for Coding</h1>');
    //console.log(req.cookies);
    //res.cookie('User_id',394);
-    return res.render('home',{
-        title:'Home',
 
-    });
+   // return res.render('home',{
+   //  title:'My social Media|Home'
+    
+   // });
+
+//const post_ = await Post.find();
+//console.log(post_,":")
+
+
+try {
+   let posts = await Post.find({}).populate("user");
+
+  return res.render("home" , {
+           title: "Codeial", 
+           posts: posts,
+        });
+} catch (err) {
+   console.log(err);
+   return ;
+}
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -11,7 +11,7 @@ module.exports.create = function(req,res){
     //     }
     //     return res.redirect('back');
     // });
-
+    console.log(req.user);
     Post.create({ 
         content:req.body.content,
         user: req.user._id})
