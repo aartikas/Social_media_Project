a8d5b0d49a4776ceef919ffff116ac6a2930d6e9
aartikas <aartika15082@iiitd.ac.in>
2023-10-19 15:22:49 +0530

    Deleting of Comments after authentication

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,16 +1,8 @@
-const Post = require('../models/posts')
+const Post = require('../models/posts');
+const Comment = require('../models/comment');
 
 module.exports.create = function(req,res){
-    // Post.create({
-    //     content:req.body.content,
-    //     user: req.user._id
-    // }, function(err,post){
-    //     if(err){
-    //         console.log("Error in creating a post:",err);
-    //         return;
-    //     }
-    //     return res.redirect('back');
-    // });
+
     console.log(req.user);
     Post.create({ 
         content:req.body.content,
@@ -23,4 +15,30 @@ module.exports.create = function(req,res){
         return;
      })
      
+};
+
+
+module.exports.destroy = async function(req,res){
+    try{
+        
+        const post= await Post.findByIdAndRemove(req.params.id);
+        
+        //.id for converting id to string
+        if(post){
+
+            let deleted= await Comment.deleteMany({post: req.params.id});
+            if(deleted){
+                console.log("The post deleted",deleted);
+                return res.redirect('back');
+            } 
+        }
+        else{
+            return res.redirect('back');
+        }
+
+    }
+    catch(err){
+        console.log("error in comment delete",err);
+        return;
+    }
 };
