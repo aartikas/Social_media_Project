26517835016f90dd9bbd324baa69aebd42474730
aartikas <aartika15082@iiitd.ac.in>
2023-11-01 15:29:11 +0530

    Adding and Dele

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -10,9 +10,10 @@
                 url: '/posts/create',
                 data: newPostForm.serialize(),
                 success: function (data) {
-                    console.log(data);
-                    // let newPost = newPostDom(data.data.post);
-                    // $('#posts-list-container>ul').prepend(newPost);
+                    //console.log(data);
+                    let newPost = newPostDom(data.data.post);
+                    $('#post-list-container>ul').prepend(newPost);
+                    deletePost($(' .delete-post-botton'),newPost);
                 },
                 error: function (err) { console.log(err.responseText); }
             });
@@ -21,8 +22,65 @@
     }
 
     //Method to create a post in DOM
+    let newPostDom = function(post){
+        //console.log(post.user);
+        //console.log(post.user.name);
+        return $(`
+                <li id="post-${post._id}">
+                <p>
+
+                    <small>
+                            <a class="delete-post-botton" href ="/posts/destroy/${post._id}">X</a>
+                    </small>
+
+                    <li>${post.content}
+                    <br>
+                    <small>${post.user.name}
+                    </small>
+                    </li>
+                    </p>
+                    <div class="post-comment">
+                
+                            <form action="/comments/create" method="POST">
+                            <input type="text" name="content" placeholder="Add Comments.." required>
+                            <input type="hidden" name="post" value="${post._id}">
+                            <input type="submit" value="Add Comment">
+                            </form>
+                    </div>
+                
+                    <div class="post-comments-list">
+                            <ul id="post-comments-${post._id}">
+  
+
+                            </ul>
+                    </div>
+                
+                </li>
+        `);
+    }
 
 
+    //Method to delete a post formDOM
+    let deletePost = function(deleteLink){
+
+        $(deleteLink).click(function(e){
+            e.preventDefault();
+            $.ajax({
+                type:'get',
+                url:$(deleteLink).prop('href'),
+                success: function(data){
+                    //console.log(data);
+                    //console.log(data._id);
+
+                    $(`#post-${data.data.post._id}`).remove();
+                },
+                error: function(data){
+                    console.log(error.responseText);
+                }
+            })
+
+        })
+    }
 
 
 
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -2,23 +2,28 @@ const Post = require('../models/posts');
 const Comment = require('../models/comment');
 
 module.exports.create = async function(req,res){
-
+try{
     //console.log(req.user);
     let post= await Post.create({ 
         content:req.body.content,
         user: req.user._id});
     
         if(req.xhr){
+            post= await post.populate('user','name');
             return res.status(200).json({
                 data:{
                     post:post
+
                 },
                 message:"Post created!"
             });
         }
-
         return res.redirect('back');
-   
+       
+    }catch(err){
+        console.log(err);
+        return res.redirect('back');
+    }
    
         // console.log("Error in creating a post:",err);
         // return;
@@ -31,12 +36,23 @@ module.exports.destroy = async function(req,res){
     try{
         
         const post= await Post.findByIdAndRemove(req.params.id);
-        console.log("Post to be deleted is ",post);
+        //console.log("Post to be deleted is ",post);
         
         //.id for converting id to string
         if(post){
 
             let deleted= await Comment.deleteMany({post: req.params.id});
+
+
+
+            if(req.xhr){
+                return res.status(200).json({
+                    data:{
+                        post_id:req.params.id
+                    },
+                    message: "Post deleted successfully"
+                })
+            }
             if(deleted){
                 console.log("The post deleted",deleted);
                 return res.redirect('back');
